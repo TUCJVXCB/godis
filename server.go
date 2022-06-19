fdb8ba294e18f81da79c0ef1649b2617fa0b701b
tanyu <838806482@qq.com>
2022-06-19 21:10:31 +0800

    init

---
+++ server.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
+
+func ListenAndServe(address string) {
+	listen, err := net.Listen("tcp", address)
+	if err != nil {
+		log.Fatal(fmt.Sprintf("listen err: %v", err))
+	}
+	defer listen.Close()
+	log.Println(fmt.Sprintf("bind:%s, start listening..", address))
+	for {
+		conn, err := listen.Accept()
+		if err != nil {
+			log.Fatal(fmt.Sprintf("accept err: %v", err))
+		}
+		go Handle(conn)
+	}
+}
+
+func Handle(conn net.Conn) {
+	reader := bufio.NewReader(conn)
+	for {
+		msg, err := reader.ReadString('\n')
+		if err == io.EOF {
+			log.Println("connection close")
+		} else {
+			log.Println(err)
+		}
+		conn.Write([]byte(msg))
+	}
+}
+
+func main() {
+	ListenAndServe(":8080")
+}
